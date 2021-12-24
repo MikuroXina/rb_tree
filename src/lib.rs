06a4d40707ef12259a0c1ef139096a6597c2d14e
MikuroXina <ryosukadnak@gmail.com>
2021-12-24 18:23:36 +0900

    Add remove_node

--- src/lib.rs
+++ src/lib.rs
@@ -260,6 +260,126 @@ impl<K: Ord, V> RedBlackTree<K, V> {
             return;
         }
     }
+    fn remove_node(&mut self, node: NonNull<Node<K, V>>) -> (K, V) {
+        if self.root == Some(node) {
+            return unsafe { Box::from_raw(self.root.take().unwrap().as_ptr()) }.into_element();
+        }
+
+        fn pop_then_promote<K, V>(node: NonNull<Node<K, V>>, child: Ptr<Node<K, V>>) -> (K, V) {
+            if let Some(parent) = Node::parent(node) {
+                Node::set_child(parent, Node::index_on_parent(node).unwrap(), child);
+            }
+            unsafe { Box::from_raw(node.as_ptr()) }.into_element()
+        }
+
+        let element = match (
+            Node::child(node, ChildIndex::Left),
+            Node::child(node, ChildIndex::Right),
+        ) {
+            (None, None) => return pop_then_promote(node, None),
+            (None, Some(right)) => return pop_then_promote(node, Some(right)),
+            (Some(left), None) => return pop_then_promote(node, Some(left)),
+            (Some(left), Some(right)) => {
+                let mut min_in_right = right;
+                while let Some(lesser) = Node::child(min_in_right, ChildIndex::Left) {
+                    min_in_right = lesser;
+                }
+                Node::set_child(
+                    Node::parent(min_in_right).unwrap(),
+                    Node::index_on_parent(min_in_right).unwrap(),
+                    None,
+                );
+                Node::set_color(min_in_right, Node::color(node));
+                Node::set_child(min_in_right, ChildIndex::Left, Some(left));
+                let right_top = if min_in_right == right {
+                    None
+                } else {
+                    Some(right)
+                };
+                Node::set_child(min_in_right, ChildIndex::Right, right_top);
+                pop_then_promote(node, Some(min_in_right))
+            }
+        };
+
+        // re-balance
+        let mut node = node;
+        while let Some(parent) = Node::parent(node) {
+            let sibling = Node::sibling(node).unwrap();
+            let close_nephew = Node::close_nephew(node);
+            let distant_nephew = Node::distant_nephew(node);
+            if Node::is_red(sibling) {
+                // if the sibling is red, the parent and the nephews are black:
+                //       [parent]
+                //        /   \
+                //      node (sibling)
+                //            /    \
+                // [close_nephew] [distant_nephew]
+                self.rotate(parent, !Node::index_on_parent(node).unwrap());
+                Node::set_color(parent, Color::Red);
+                Node::set_color(sibling, Color::Black);
+                // then:
+                //       [sibling]
+                //        /   \
+                //   (parent) [distant_nephew]
+                //    /    \
+                // node [close_nephew]
+                continue;
+            }
+            if distant_nephew.map_or(false, Node::is_red) {
+                // if the sibling is black and the distant nephew is red:
+                //   parent
+                //    /  \
+                // node [sibling]
+                //         \
+                //    (distant_nephew)
+                self.rotate(parent, !Node::index_on_parent(node).unwrap());
+                Node::set_color(sibling, Node::color(parent));
+                Node::set_color(parent, Color::Black);
+                Node::set_color(distant_nephew.unwrap(), Color::Black);
+                // then:
+                //      sibling
+                //       /  \
+                // [parent] [distant_nephew]
+                //     /
+                //   node
+                break;
+            }
+            if close_nephew.map_or(false, Node::is_red) {
+                // if the sibling is black and the close nephew is red:
+                //        parent
+                //         /  \
+                //      node [sibling]
+                //             /   \
+                // (close_nephew) [distant_nephew]
+                self.rotate(sibling, Node::index_on_parent(node).unwrap());
+                Node::set_color(sibling, Color::Red);
+                Node::set_color(close_nephew.unwrap(), Color::Black);
+                // then:
+                //   parent
+                //    /  \
+                // node [close_nephew]
+                //         \
+                //      (sibling)
+                continue;
+            }
+            if Node::color(parent) == Color::Red {
+                // if the parent is red, the sibling and the nephews are black:
+                //       (parent)
+                //        /   \
+                //      node [sibling]
+                //            /    \
+                // [close_nephew] [distant_nephew]
+                Node::set_color(sibling, Color::Red);
+                Node::set_color(parent, Color::Black);
+                break;
+            }
+            // if the parent and sibling and nephews are all black:
+            Node::set_color(sibling, Color::Red);
+            // the parent node needs to re-balance.
+            node = parent;
+        }
+        element
+    }
 }
 
 impl<K, V> Drop for RedBlackTree<K, V> {
