684b7edb124941967bc9e9135768b86382afb2da
MikuroXina <ryosukadnak@gmail.com>
2021-12-30 23:51:50 +0900

    Fix to use remove on test

--- src/tests.rs
+++ src/tests.rs
@@ -9,11 +9,11 @@ fn simple() {
     tree.insert(3, 'd');
     tree.insert(5, 'e');
 
-    assert_eq!(tree.get(&0), None);
-    assert_eq!(tree.get(&1), Some(&'a'));
-    assert_eq!(tree.get(&2), Some(&'c'));
-    assert_eq!(tree.get(&3), Some(&'d'));
-    assert_eq!(tree.get(&4), Some(&'b'));
-    assert_eq!(tree.get(&5), Some(&'e'));
-    assert_eq!(tree.get(&6), None);
+    assert_eq!(tree.remove(&0), None);
+    assert_eq!(tree.remove(&1), Some('a'));
+    assert_eq!(tree.remove(&2), Some('c'));
+    assert_eq!(tree.remove(&3), Some('d'));
+    assert_eq!(tree.remove(&4), Some('b'));
+    assert_eq!(tree.remove(&5), Some('e'));
+    assert_eq!(tree.remove(&6), None);
 }
