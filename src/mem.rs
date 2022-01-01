900ff1082cc88ee04907388efa7fff4294d2895a
MikuroXina <ryosukadnak@gmail.com>
2022-01-02 00:15:13 +0900

    Remove replace

--- src/mem.rs
+++ src/mem.rs
@@ -1,17 +1,5 @@
 use crate::node::NodeRef;
 
-pub fn replace<T, R>(v: &mut T, change: impl FnOnce(T) -> (T, R)) -> R {
-    use std::{panic, ptr};
-
-    unsafe {
-        let old_t = ptr::read(v);
-        let (new_t, r) = panic::catch_unwind(panic::AssertUnwindSafe(|| change(old_t)))
-            .unwrap_or_else(|_| ::std::process::abort());
-        ptr::write(v, new_t);
-        r
-    }
-}
-
 pub struct NodeDropGuard<K, V, const N: usize>(pub [NodeRef<K, V>; N]);
 
 impl<K, V, const N: usize> Drop for NodeDropGuard<K, V, N> {
