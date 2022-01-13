653f8e0f7493aa3c479638b2bbc7c8685e6ac39d
MikuroXina <ryosukadnak@gmail.com>
2022-01-13 21:56:11 +0900

    Add simple_remove

--- src/balance/tests.rs
+++ src/balance/tests.rs
@@ -614,3 +614,84 @@ fn simple_insert() {
         assert!(node5.is_red());
     }
 }
+
+#[test]
+fn simple_remove() {
+    let mut tree = RedBlackTree::new();
+    tree.insert(1, ());
+    tree.insert(4, ());
+    tree.insert(2, ());
+    tree.insert(3, ());
+    tree.insert(5, ());
+
+    tree.remove(&1);
+    //   (4)
+    //   / \
+    // [2] [5]
+    //   \
+    //   (3)
+    {
+        let node4 = tree.root.unwrap();
+        assert_eq!(node4.key(), &4);
+        assert!(node4.is_red());
+        let (node2, node5) = node4.children();
+        let node2 = node2.expect("node2 must exist");
+        let node5 = node5.expect("node5 must exist");
+
+        assert_eq!(node2.key(), &2);
+        assert!(node2.is_black());
+        let (_, node3) = node2.children();
+        let node3 = node3.expect("node3 must exist");
+
+        assert_eq!(node3.key(), &3);
+        assert!(node3.is_red());
+
+        assert_eq!(node5.key(), &5);
+        assert!(node5.is_black());
+    }
+
+    tree.remove(&2);
+    //   (4)
+    //   / \
+    // [3] [5]
+    {
+        let node4 = tree.root.unwrap();
+        assert_eq!(node4.key(), &4);
+        assert!(node4.is_red());
+        let (node3, node5) = node4.children();
+        let node3 = node3.expect("node3 must exist");
+        let node5 = node5.expect("node5 must exist");
+
+        assert_eq!(node3.key(), &3);
+        assert!(node3.is_red());
+
+        assert_eq!(node5.key(), &5);
+        assert!(node5.is_black());
+    }
+
+    tree.remove(&3);
+    // [4]
+    //   \
+    //   (5)
+    {
+        let node4 = tree.root.unwrap();
+        assert_eq!(node4.key(), &4);
+        assert!(node4.is_black());
+        let (_, node5) = node4.children();
+        let node5 = node5.expect("node5 must exist");
+
+        assert_eq!(node5.key(), &5);
+        assert!(node5.is_red());
+    }
+
+    tree.remove(&4);
+    // [5]
+    {
+        let node5 = tree.root.unwrap();
+        assert_eq!(node5.key(), &5);
+        assert!(node5.is_black());
+    }
+
+    tree.remove(&5);
+    assert!(tree.is_empty());
+}
